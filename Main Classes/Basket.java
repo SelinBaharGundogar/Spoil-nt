2198029eeb9f1ce92b4b82f657b34d4286ed0815
SelinBaharGundogar <82098424+SelinBaharGundogar@users.noreply.github.com>
2021-07-18 14:07:38 +0300

    Half of it is complete
    Comparable interface has to be completed in order to finish this class + documentation needs to be added

---
+++ Main Classes/Basket.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+/**
+ * This  class creates a basket.
+ * @version 18 July, 2021
+ * @author Selin Bahar Gündoğar
+ */
+public class Basket implements Comparable{
+    private ArrayList<CurrentItem> products;
+    public Basket() {
+        products = new ArrayList<CurrentItem>();
+
+    }
+
+    public void removeSelected() {
+        for ( int count = 0; count < products.size(); count++ ) {
+            if ( products.get(count).isSelected() == true ) {
+                products.remove(count);
+            }
+        }
+    }
+
+    public void addItem(Item newItem) {
+        products.add(newItem);
+    }
+
+    //Todo after the Comparable interface is written
+    @Override
+    public int compareTo(Object o) {
+        return 0;
+    }
+
+}
