18b5818bee4a44de8d3aac17953e2cc3823cbcda
SelinBaharGundogar <82098424+SelinBaharGundogar@users.noreply.github.com>
2021-08-03 13:07:53 +0300

    Delete src directory

--- src/sample/Test2.java
+++
@@ -1,9 +0,0 @@
-package sample;
-
-public class Test2 {
-    public static void main(String[] args) {
-       // System.out.println(UserContainer.getCustomerUsersList());
-        User user = new CustomerUser("", "",1,"","ahmet") ;
-        System.out.println(user.getUsername());
-    }
-}
--- src/sample/TheUser.java
+++
@@ -1,5 +0,0 @@
-package sample;
-
-public interface TheUser {
-    public User currentUser();
-}
